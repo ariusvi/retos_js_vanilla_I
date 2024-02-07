25754a2b970bd4d6d3c8dc35e90258bf0a06bdc2
Ana Rius <a.riusvi11@gmail.com>
2024-02-07 20:10:26 +0100

    1. Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor de los dos. Si son iguales indicarlo también. Ves cambiando los valores para comprobar que funciona.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/* 
+1. Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor de los
+dos. Si son iguales indicarlo también. Ves cambiando los valores para comprobar que
+funciona.
+*/
+
+let numA = 5
+let numB = 10
+
+if(numA > numB) {
+    console.log("el primer número es mayor que el segundo número");
+} else if (numA < numB) {
+    console.log("el primer número es menor que el segundo");
+} else if (numA === numB) {
+    console.log("ambos números son iguales");
+}
